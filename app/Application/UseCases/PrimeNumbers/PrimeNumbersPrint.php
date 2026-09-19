72bd084b2b99a7e71c3a15f0d8ab09b66010b079
Bel Vica <gaelysm3@gmail.com>
2026-09-19 11:41:34 +0200

    TDD - Refactor continue passing test

--- app/Application/UseCases/PrimeNumbers/PrimeNumbersPrint.php
+++ app/Application/UseCases/PrimeNumbers/PrimeNumbersPrint.php
@@ -6,29 +6,46 @@ namespace App\Application\UseCases\PrimeNumbers;
 
 class PrimeNumbersPrint
 {
-    public function execute(int $number): array
+    /**
+     * Cambio al algoritmo de la Criba de Eratóstenes para mejorar la eficiencia en la generación de números primos.
+     * Este método genera todos los números primos hasta un límite dado utilizando la Criba de Eratóstenes, que es más eficiente que verificar cada número individualmente.
+     */
+    public function execute(int $limit): array
     {
-        if ($number < 2) {
+        if ($limit < 2) {
             return [];
         }
 
-        $primes = [];
-        for ($i = 2; $i <= $number; $i++) {
-            if ($this->isPrime($i)) {
-                $primes[] = $i;
+        $sieve = array_fill(0, $limit + 1, true);
+        $sieve[0] = $sieve[1] = false;
+
+        for ($i = 2; $i * $i <= $limit; $i++) {
+            if ($sieve[$i]) {
+                for ($j = $i * $i; $j <= $limit; $j += $i) {
+                    $sieve[$j] = false;
+                }
             }
         }
 
-        return $primes;
+        return array_keys(array_filter($sieve));
     }
 
+    // DEPRECATED
     private function isPrime(int $number): bool
     {
         if ($number < 2) {
             return false;
         }
 
-        for ($i = 2; $i <= sqrt($number); $i++) {
+        if ($number === 2) {
+            return true;
+        }
+
+        if ($number % 2 === 0) {
+            return false;
+        }
+
+        for ($i = 3; $i <= sqrt($number); $i += 2) {
             if ($number % $i === 0) {
                 return false;
             }
